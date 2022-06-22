9d7aae5b0cfb75bed12293f5dba2e3cd7c27f5fc
Paola <paola@Paolas-MacBook-Pro.local>
2022-06-21 21:02:34 -0500

    Sesion 1 semana 2

---
+++ Python/Sem-2/Listas.py
@@ -0,0 +1,49 @@
+## ejemplo de listas en python 
+
+#Creando una lista
+nombres = ["Paola", "Carlos", "Mario", "Israel", "Manuel", "Luis", "Guillermo"]
+
+# Accediendo y pintando un elemento con x posicion de la lista
+print(nombres[1])
+
+# Cambiando un elemento con x posicion en la lista
+nombres[0]="Elizabeth"
+
+# Agregar elementos a la lista
+
+nombres.insert(7, "Juan")
+nombres.insert(1, "Pedrita")
+
+nombres.append("Perenganita")
+##nombres.("sutanita")
+
+print(nombres)
+
+# Despues de dos meses 
+
+nombres.remove("Juan")
+
+
+#Barrer una lista
+#Para barrer una lista, se requiere un loop 
+#FOR 
+#WHILE 
+#DO-WHILE
+#FOREACH 
+
+
+for x in nombres:
+    print(x)
+
+
+
+nombres.pop(1)
+
+print(nombres)
+
+
+for y in range(1):
+    print(nombres[y])
+
+print(len(nombres))
+
